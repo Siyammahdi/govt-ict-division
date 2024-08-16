70386f192fac868aa76d883afa3d3f953501fd5d
Siyammahdi <siyammahdi1@gmail.com>
2024-08-16 16:03:01 +0600

    typescript error solved

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,12 +1,12 @@
-// pages/_app.js
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
     </NextUIProvider>
-  )
+  );
 }
 
 export default MyApp;
